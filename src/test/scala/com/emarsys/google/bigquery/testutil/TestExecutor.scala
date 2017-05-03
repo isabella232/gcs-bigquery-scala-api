5f25a8206b26b58d4ed4dbf6a9d7a6614b341b4d
fawalmargit <margit.fawal@emarsys.com>
2017-05-03 09:44:45 +0200

    Added TestExecutor.

---
+++ src/test/scala/com/emarsys/google/bigquery/testutil/TestExecutor.scala
@@ -0,0 +1,52 @@
+package com.emarsys.google.bigquery.testutil
+
+
+import com.emarsys.google.bigquery._
+import com.emarsys.google.bigquery.model.BqQueryJobConfig
+import com.google.api.services.bigquery.model.{ErrorProto, Job, JobReference, JobStatus}
+import org.joda.time.LocalDate
+
+import scala.concurrent.{ExecutionContext, Future}
+
+trait TestExecutor extends BigQueryExecutor {
+
+  var commandsReceived = Seq.empty[TableCommand[_]]
+
+  val oneTableErrorDate = LocalDate.parse("2016-10-10")
+
+  def resetCommands() = commandsReceived = Seq()
+
+  override def execute[T](command: TableCommand[_])(implicit ec: ExecutionContext): Future[T] = command match {
+    case CopyCommand(_, jobConfig) if isOpenQueryOnErrorDate(jobConfig) =>
+      Future.successful(createJob(JobStatusChecker.STATUS_ERROR).asInstanceOf[T])
+    case JobStatusCommand(cmd) if cmd.getJobId == "project:errorId" =>
+      Future.successful(createJob(JobStatusChecker.STATUS_ERROR).asInstanceOf[T])
+    case _ =>
+      if (!command.isInstanceOf[JobStatusCommand]) {
+        commandsReceived +:= command
+      }
+      Future.successful(createJob(JobStatusChecker.STATUS_DONE).asInstanceOf[T])
+  }
+
+  def isOpenQueryOnErrorDate(jobConfig: BqQueryJobConfig) = {
+    jobConfig.query.show.contains("opens_" + oneTableErrorDate.toString("yyyyMMdd"))
+  }
+
+  def createJob(status: String) = {
+    val job       = new Job()
+    val jobStatus = new JobStatus()
+    jobStatus.setState(status)
+    job.setId("project:jobId")
+    if (status == JobStatusChecker.STATUS_ERROR) {
+      job.setId("project:errorId")
+      val errorProto = new ErrorProto().setDebugInfo("test error")
+      jobStatus.setErrorResult(errorProto)
+    }
+    job.setStatus(jobStatus)
+    job.setJobReference(new JobReference().setJobId(job.getId))
+    job
+  }
+
+}
+
+trait TestAsyncExecutor extends BigQueryAsyncExecutor with TestExecutor

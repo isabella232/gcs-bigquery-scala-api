328552f05b43ae43645e6198780e7927261d9e7e
Gyorgy Hamar <gyurinak@gmail.com>
2019-03-20 15:43:27 +0100

    SEG1-69 Modify BigQueryJobResult so that affectedRows is a BigInt

--- src/main/scala/com/emarsys/google/bigquery/model/BigQueryJobModel.scala
+++ src/main/scala/com/emarsys/google/bigquery/model/BigQueryJobModel.scala
@@ -2,7 +2,7 @@ package com.emarsys.google.bigquery.model
 
 object BigQueryJobModel {
 
-  case class BigQueryJobResult(affectedRows: Option[Long])
+  case class BigQueryJobResult(affectedRows: BigInt)
 
   case class BigQueryJobError(message: String, reason: String, location: String, table: String)
 
